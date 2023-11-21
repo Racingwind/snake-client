0fcceb15f3e6c75ae8c04f548a4e9b9bbb466f8f
Racingwind <louis.k.hu@gmail.com>
2023-11-21 02:12:01 -0800

    added user input handling. ensured ctrl+c terminates the program

--- play.js
+++ play.js
@@ -1,7 +1,24 @@
 const client = require("./client");
 
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
 
 
 console.log("Connecting ...");
 client.connect();
-
+setupInput();
